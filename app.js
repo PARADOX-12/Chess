20030239e0bcc3481ea458a8e8ce6cc88d6c3d34
Sandeep Kumar <104671195+PARADOX-12@users.noreply.github.com>
2024-10-04 19:58:09 +0530

    add vercel.jso file

--- app.js
+++ app.js
@@ -70,6 +70,6 @@ io.on("connection", (uniqueSocket) => {
 });
 
 
-server.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`);
-});
+server.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running on port", process.env.PORT || 3000);
+  });
